a811736718d3b2f6bcc1b5c4b52f0251aaaffa9c
Yusuf Ibrahim <yusufmaras2005@gmail.com>
2026-06-29 08:48:52 +0300

    edited close , adjclose price

--- services/timeMachine.service.js
+++ services/timeMachine.service.js
@@ -28,7 +28,7 @@ const getDailyTimeSeries = async (obj) => {
   data.forEach((day) => {
     const dateOnly = day.date.split("T")[0];
 
-    const closePrice = day.adjClose;
+    const closePrice = day.close;
 
     priceData.push({
       tarih: dateOnly,
